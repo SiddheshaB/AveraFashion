761dbcfcad4c6e2f4b458568b8b87550bef91e87
SiddheshaB <siddhesha.bahira@gmail.com>
2024-12-08 19:34:02 +0000

    LoginCompleted

--- App.js
+++ App.js
@@ -2,19 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 import React, { useState } from "react";
 import Login from "./components/Auth.native";
+import HomeScreen from "./app/(tabs)/index";
 
 export default function App() {
   console.log("Hello App file");
-  return (
-    <View style={styles.container}>
+  return <HomeScreen></HomeScreen>;
+
+  {
+    /* <View style={styles.container}>
       <Text>Welcome to StyleMe App...</Text>
-      <View style={styles.loginBox}>
-        <Login />
-      </View>
 
       <StatusBar style="auto" />
-    </View>
-  );
+    </View> */
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>This is homescreen.. :)</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
